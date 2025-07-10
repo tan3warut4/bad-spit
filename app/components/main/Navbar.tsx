f6cf7bb25b2ada06e74586747b13fcc1b9c0ccbe
tan3warut4 <itanprscritical@gmail.com>
2025-07-10 16:44:52 +0700

    nav subtt

--- app/components/main/Navbar.tsx
+++ app/components/main/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className='w-full max-w-5xl mx-auto py-4 flex gap-4 px-4'>
         <div className='font-semibold text-xl'>Badminton Split</div>
-        <div className='text-sm self-center'> แหล่งรวมเครื่องมือช่วยเล่นแบดง่ายๆ !</div>
+        <div className='text-xs text-gray-500 tracking-wider self-center'>developed by tan3warut4</div>
     </div>
   )
 }
